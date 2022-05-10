3812deabf5f26dacf723d962309b5a70c20fd557
Jenish Shah <jenishshah1604@gmail.com>
2022-05-10 16:37:13 +0530

    Wallet api created automatically on verification

--- models/wallet.js
+++ models/wallet.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
 
 const walletSchema = new Schema({
-    username: {
-        type: String,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"users",
         required: true
     },
     amount: {
