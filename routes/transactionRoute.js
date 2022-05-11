dabd840ccc628671a9326e7a19ccbb60575eadde
jenish Dhanani <202112106@daiict.ac.in>
2022-05-11 20:26:16 +0530

    changes

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,18 +1,52 @@
 const router = require('express').Router();
 const transaction = require('../models/transaction');
 let Transaction = require('../models/transaction');
+let wallet = require('../models/wallet');
+let Auction = require('../models/auction.models')
 
 router.route('/get_transaction/:id').get((req,res)=> {
-    Transaction.find({"userId":req.params.id})
+    Transaction.find({"bidderId":req.params.id})
     .then(transaction=>res.json(transaction))
     .catch(err=>res.status(400).json('Error' + err));
 });
 
 router.route('/add_transaction').post((req, res) => {
+    var amt;
+    console.log(req.body)
     const transaction = new Transaction(req.body)
     transaction.save()
     .then((result) => {
-        res.json('Transaction added')
+        wallet.findOne({userId:req.body.sellerId})
+        .then(walletobj=>{
+            console.log("seller Amount "+walletobj.amount)
+            amt=walletobj.amount+req.body.amount;
+            wallet.findOneAndUpdate({userId:req.body.sellerId},{$set:{"amount":amt}},{new: true})
+            .then(res=>
+            {
+                console.log("seller Updated "+res.amount)
+            })
+            .catch(err=>res.status(400).json('Error' + err));
+        })
+        .catch(err=>{return res.status(400).json('Error' + err)});
+
+        wallet.findOne({userId:req.body.bidderId})
+        .then(walletobj=>{
+            console.log("Bidder Amount "+walletobj.amount)
+            amt=walletobj.amount-req.body.amount;
+
+            wallet.findOneAndUpdate({userId:req.body.bidderId},{"amount": amt},{new: true})
+            .then((res)=>{
+                console.log("Bidder Updated "+res.amount)
+            })
+            .catch(err=>{return res.status(400).json('Error' + err)});
+        })
+        .catch(err=>{return res.status(400).json('Error' + err)});
+
+        //update to complete
+        Auction.findByIdAndUpdate(req.body.auctionId,{status:"completed"})
+        .catch(err=>res.status(400).json('Error' + err));
+
+        return res.json('Transaction added')
     })
 });
 
